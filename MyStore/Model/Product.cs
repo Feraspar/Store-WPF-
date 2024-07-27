095b1bd05ec655f8b64b4eb8ea76114823be7391
Feraspar <97383817+Feraspar@users.noreply.github.com>
2024-07-28 01:26:58 +0700

    Создана модель товара

---
+++ MyStore/Model/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Model
+{
+    /// <summary>
+    /// Модель товара
+    /// </summary>
+    public class Product : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    OnPropertyChanged(nameof(Price));
+                }
+            }
+        }
+
+        private string _name;
+        private decimal _price;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
