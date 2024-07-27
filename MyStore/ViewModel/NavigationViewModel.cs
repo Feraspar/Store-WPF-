b135faf8095648955b1fdb86a5b33898dc565dce
Feraspar <97383817+Feraspar@users.noreply.github.com>
2024-07-28 01:42:08 +0700

    Создан ViewModel для окна навигации и свёрстано само окно

---
+++ MyStore/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MyStore.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MyStore.ViewModel
+{
+    public class NavigationViewModel : ObservableObject
+    {
+        public UserControl CurrentView
+        {
+            get => _currentView;
+            set => SetProperty(ref _currentView, value);
+        }
+        public ICommand StorePageCommand { get; private set; }
+        public ICommand CartPageCommand { get; private set; }
+
+        private UserControl _currentView;
+
+        public NavigationViewModel()
+        {
+            StorePageCommand = new RelayCommand(NavigateToStore);
+            CartPageCommand = new RelayCommand(NavigateToCart);
+            NavigateToStore();
+        }
+
+        private void NavigateToStore()
+        {
+            CurrentView = new StorePanel();
+        }
+        private void NavigateToCart()
+        {
+            CurrentView = new CartPanel();
+        }
+    }
+}
